671ff059b9ee0a8bd9fbc6a02fcfef588d43b5ca
MW777 <MW777@users.noreply.github.com>
2014-12-07 20:43:29 +0000

    Updated to follow rules and recommendations

--- Code/shape/EvolShape.java
+++ Code/shape/EvolShape.java
@@ -1,21 +1,31 @@
-package Shapes;
+package shapes;
 
 /**
- * Requirements for each shape.
+ * A shape which represents the appearance of an Individual in Evolutionary
+ * Computation.
+ * 
+ * An EvolShape can be mutated or used in cross-over to create EvolShapes with
+ * different properties.
+ * 
+ * @author Martin Wong
+ * @version 2014-12-07
  */
 public interface EvolShape extends Shape {
 	
 	/**
-	 * Uniform Mutation.
+	 * Uniform Mutation, it alters the center or area of an EvolShape.
+	 * 
 	 * @param centerMin (PointXY)
 	 * @param centerMax (PointXY)
 	 * @param vertexMin (PointXY)
 	 * @param vertexMax (PointXY)
 	 */
-	void uniformMutation(PointXY centerMin, PointXY centerMax, PointXY vertexMin, PointXY vertexMax);
+	void uniformMutation(PointXY centerMin, PointXY centerMax,
+						 PointXY vertexMin, PointXY vertexMax);
 	
 	/**
-	 * Non-uniform Mutation.
+	 * Non-uniform Mutation, it alters the center or area of an EvolShape.
+	 * 
 	 * @param centerMin (PointXY)
 	 * @param centerMax (PointXY)
 	 * @param vertexMin (PointXY)
@@ -24,48 +34,60 @@ public interface EvolShape extends Shape {
 	 * @param maxGen (int)
 	 * @param b (double)
 	 */
-	void nonUniformMutation(PointXY centerMin, PointXY centerMax, PointXY vertexMin, PointXY vertexMax, int currentGen, int maxGen, double b);
+	void nonUniformMutation(PointXY centerMin, PointXY centerMax,
+							PointXY vertexMin, PointXY vertexMax,
+							int currentGen, int maxGen, double b);
 	
 	/**
-	 * Flat Crossover.
-	 * @param es (EvolShape)
-	 * @return offspring (EvolShape)
+	 * Generates offspring by Flat Crossover with another EvolShape.
+	 * 
+	 * @param es, the second parent (EvolShape)
+	 * @return offspring: 1 (EvolShape)
 	 */
 	EvolShape flatCrossover(EvolShape es);
 	
 	/**
-	 * Simple Crossover.
-	 * @param es (EvolShape)
-	 * @return offspring (EvolShape[])
+	 * Generates offspring by Simple Crossover with another EvolShape.
+	 * 
+	 * @param es, the second parent (EvolShape)
+	 * @return offspring: 2 (EvolShape[])
 	 */
 	EvolShape[] simpleCrossover(EvolShape es);
 	
 	/**
-	 * Whole Arithmetical Crossover.
-	 * @param es (EvolShape)
-	 * @return offspring (EvolShape[])
+	 * Generates offspring by Whole Arithmetical Crossover with another.
+	 * EvolShape.
+	 * 
+	 * @param es, the second parent (EvolShape)
+	 * @return offspring: 2 (EvolShape[])
 	 */
 	EvolShape[] wholeCrossover(EvolShape es);
 	
 	/**
-	 * Local Arithmetical Crossover.
-	 * @param es (EvolShape)
-	 * @return offspring (EvolShape[])
+	 * Generates offspring by Local Arithmetical Crossover with another
+	 * EvolShape.
+	 * 
+	 * @param es, the second parent (EvolShape)
+	 * @return offspring: 2 (EvolShape[])
 	 */
 	EvolShape[] localCrossover(EvolShape es);
 	
 	/**
-	 * Single Arithmetical Crossover.
-	 * @param es (EvolShape)
-	 * @return offspring (EvolShape[])
+	 * Generates offspring by Single Arithmetical Crossover with another
+	 * EvolShape.
+	 * 
+	 * @param es, the second parent (EvolShape)
+	 * @return offspring: 2 (EvolShape[])
 	 */
 	EvolShape[] singleCrossover(EvolShape es);
 	
 	/**
-	 * BLX-alpha Crossover.
-	 * @param es (EvolShape)
-	 * @param alpha (double)
-	 * @return offspring (EvolShape)
+	 * Generates offspring by BLX-alpha Crossover with another
+	 * EvolShape.
+	 * 
+	 * @param es, the second parent (EvolShape)
+	 * @param alpha, a constant (double)
+	 * @return offspring: 2 (EvolShape)
 	 */
 	EvolShape blxAphaCrossover(EvolShape es, double alpha);
 
