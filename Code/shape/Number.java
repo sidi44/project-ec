efbf812133b5241a28d7030df2a1fe8d4dda2b7d
MW777 <MW777@users.noreply.github.com>
2014-12-07 20:55:20 +0000

    Updated to follow rules and recommendations

--- Code/shape/Number.java
+++ Code/shape/Number.java
@@ -1,15 +1,20 @@
-package Shapes;
+package shapes;
 
 import java.util.Arrays;
 import java.util.Random;
 
 /**
- * Class for random number generation and number manipulation.
+ * Class with static methods for random number generation and
+ * number manipulation.
+ * 
+ * @author Martin Wong
+ * @version 2014-12-07
  */
 public class Number {
 	
 	/**
-	 * Sort doubles into ascending order.
+	 * Sort two numbers into ascending order.
+	 * 
 	 * @param num1 (double)
 	 * @param num2 (double)
 	 * @return sorted numbers (double[])
@@ -22,10 +27,13 @@ public class Number {
 	}
 	
 	/**
-	 * Generates a random int within int boundaries.
+	 * Generates a random int within int boundaries (inclusive) provided.
+	 * The lower-bound and upper-bound ints can be provided in any order, as
+	 * they will be sorted prior to random number generation.
+	 * 
 	 * @param boundary1 (int)
 	 * @param boundary2 (int)
-	 * @return random int in range (int)
+	 * @return random int in within the boundaries (int)
 	 */
 	public static int randomInt(int boundary1, int boundary2){
 		double[] sorted = sort(boundary1, boundary2);
@@ -34,11 +42,14 @@ public class Number {
 		int max = (int) sorted[1];
 		
 		Random r = new Random();
-		return r.nextInt((max - min) + 1) + min; // +1 for inclusive
+		return r.nextInt((max - min) + 1) + min; // + 1 for inclusive
 	}
 	
 	/**
-	 * Generates a random double within double boundaries.
+	 * Generates a random double within double boundaries (inclusive) provided.
+	 * The lower-bound and upper-bound doubles can be provided in any order, as
+	 * they will be sorted prior to random number generation.
+	 * 
 	 * @param boundary1 (double)
 	 * @param boundary2 (double)
 	 * @return random double in range (double)
