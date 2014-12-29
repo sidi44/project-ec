32afb7af0828492671e8e7eb870aa4cf838f6690
MW777 <MW777@users.noreply.github.com>
2014-12-29 00:57:42 +0000

    Updated to follow rules and recommendations

--- Code/shape/Shape.java
+++ Code/shape/Shape.java
@@ -1,25 +1,25 @@
-package shapes;
+package shape;
 
 /**
  * Defines the properties which a shape must have.
  * 
  * @author Martin Wong
- * @version 2014-12-14
+ * @version 2014-12-28
  */
 public interface Shape {
 
 	/**
-	 * The reference point for other vertices (if any).
+	 * The reference point for the shape.
 	 * 
 	 * @return reference point of shape (PointXY)
 	 */
-	public PointXY getReference();
+	public PointXY getReference ();
 	
 	/**
 	 * Calculates the area of a shape.
 	 * 
 	 * @return area of shape (double)
 	 */
-	public double getArea();
+	public double getArea ();
 
 }
