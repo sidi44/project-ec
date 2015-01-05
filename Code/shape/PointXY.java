f23c8abc528bf076c6c677f0482bfb29b30c9650
MW777 <MW777@users.noreply.github.com>
2015-01-05 22:04:56 +0000

    Updated to follow rules and recommendations

--- Code/shape/PointXY.java
+++ Code/shape/PointXY.java
@@ -4,14 +4,20 @@ package shape;
  * Represents an (x, y) co-ordinate.
  * 
  * @author Martin Wong
- * @version 2014-12-28
+ * @version 2015-01-05
  */
 public class PointXY {
 	
 	private double x;
 	private double y;
 	
-	public PointXY (double x, double y) {
+	/**
+	 * Creates an instance of PointXY, with co-ordinates (x, y).
+	 * 
+	 * @param x (double)
+	 * @param y (double)
+	 */
+	public PointXY(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -21,7 +27,7 @@ public class PointXY {
 	 * 
 	 * @return x (double)
 	 */
-	public double getX () {
+	public double getX() {
 		return this.x;
 	}
 	
@@ -30,7 +36,7 @@ public class PointXY {
 	 * 
 	 * @return y (double)
 	 */
-	public double getY () {
+	public double getY() {
 		return this.y;
 	}
 	
@@ -39,7 +45,7 @@ public class PointXY {
 	 * 
 	 * @param x (double)
 	 */
-	public void setX (double x) {
+	public void setX(double x) {
 		this.x = x;
 	}
 	
@@ -48,7 +54,7 @@ public class PointXY {
 	 * 
 	 * @param y (double)
 	 */
-	public void setY (double y) {
+	public void setY(double y) {
 		this.y = y;
 	}
 	
@@ -58,7 +64,7 @@ public class PointXY {
 	 * @param x (double)
 	 * @param y (double)
 	 */
-	public void setXY (double x, double y) {
+	public void setXY(double x, double y) {
 		this.x = x;
 		this.y = y;
 	}
@@ -69,7 +75,7 @@ public class PointXY {
 	 * @param point (PointXY)
 	 * @return distance (double)
 	 */
-	public double getDistance (PointXY point) {
+	public double getDistance(PointXY point) {
 		double a = point.getX() - this.x;
 		double b = point.getY() - this.y;
 		double result = (a * a) + (b * b);
@@ -82,7 +88,7 @@ public class PointXY {
 	 * @return PointXY in the form: "(x, y)" (string)
 	 */
 	@Override
-	public String toString () {
+	public String toString() {
 		return "(" + getX() + ", " + getY() + ")";
 	}
 
